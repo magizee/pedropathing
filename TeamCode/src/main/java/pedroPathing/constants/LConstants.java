b4ef9d4cbdfbb7682e9baf82c07c368c7916f9b9
maggie zhu <107164272+magizee@users.noreply.github.com>
2025-01-07 16:24:55 -0500

    12

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -5,9 +5,9 @@ import com.pedropathing.localization.constants.*;
 
 public class LConstants {
     static {
-        ThreeWheelConstants.forwardTicksToInches = .001989436789;
-        ThreeWheelConstants.strafeTicksToInches = .001989436789;
-        ThreeWheelConstants.turnTicksToInches = .001989436789;
+        ThreeWheelConstants.forwardTicksToInches = 0.00052; //lower it is, lower the telemetry value will be
+        ThreeWheelConstants.strafeTicksToInches = 0.00052;
+        ThreeWheelConstants.turnTicksToInches = 0.00052;
         ThreeWheelConstants.leftY = 6.125;
         ThreeWheelConstants.rightY = -6.125;
         ThreeWheelConstants.strafeX = -4.4;
@@ -16,7 +16,7 @@ public class LConstants {
         ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightRear";
         ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
         ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
-        ThreeWheelConstants.strafeEncoderDirection = Encoder.REVERSE;
+        ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
     }
 }
 
